dd051e369cd42d9557a38564410c9aeb9db75526
Fabian Lee <flee999@hotmail.com>
2021-10-08 23:25:18 -0400

    agent=false for tf remote-connection

--- tf/jumpbox.tf
+++ tf/jumpbox.tf
@@ -20,8 +20,10 @@ resource "digitalocean_droplet" "jumpbox1" {
     host = self.ipv4_address
     user = "root"
     type = "ssh"
+    # had errors until using agent=false, "Error connecting to SSH_AUTH_SOCK: dial unix"
+    agent = false
     private_key = file(var.pvt_key)
-    timeout = "2m"
+    timeout = "5m"
   }
 
   provisioner "remote-exec" {
